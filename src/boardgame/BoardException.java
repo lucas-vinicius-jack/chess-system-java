c1501907b659abbcc821a26259104db2a5eec3e4
lucas-vinicius-jack <lucasvinicius1657@gmail.com>
2022-03-05 16:59:51 -0300

    Piece move count

---
+++ src/boardgame/BoardException.java
@@ -0,0 +1,14 @@
+package boardgame;
+
+public class BoardException extends RuntimeException{
+
+    private static final long serialVersion = 1L;
+    
+    public BoardException(String msg){
+        super(msg);
+    }
+    
+    
+    
+    
+}
